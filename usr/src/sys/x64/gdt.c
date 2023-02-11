8a2ae2be86b594b00c41407f122112f4965ebc8f
Alan Potteiger <alan@mkall.org>
2023-02-11 03:45:23 +0100

    Work on GDT

--- usr/src/sys/x64/gdt.c
+++ usr/src/sys/x64/gdt.c
@@ -9,12 +9,34 @@
 
 #include <stdint.h>
 
+#include <sys/x64/gdt.h>
+
+/* Global Descriptor table */
+Segdesc GDT[6];
+
 /*
  * Initialize the Global Descriptor Table
  */
 void
 gdt_init(void)
 {
+	/* Null descriptor. */
+	GDT[0] = 0;
+
+	/* Kernel code segment. */
+	GDT[1] = SEGDESC_TYPE(1) | SEGDESC_RW(1) | SEGDESC_DPL(0)
+		| SEGDESC_PRES(1) | SEGDESC_LONG(1);
+	/* Kernel data segment. */
+	GDT[2] = SEGDESC_TYPE(1) | SEGDESC_RW(1) | SEGDESC_DPL(0)
+		| SEGDESC_PRES(1) | SEGDESC_LONG(1);
+
+	/* User code segment. */
+	GDT[3] = SEGDESC_TYPE(1) | SEGDESC_RW(1) | SEGDESC_DPL(3)
+		| SEGDESC_PRES(1) | SEGDESC_LONG(1);
+	/* User data segment. */
+	GDT[4] = SEGDESC_TYPE(1) | SEGDESC_RW(1) | SEGDESC_DPL(3)
+		| SEGDESC_PRES(1) | SEGDESC_LONG(1);
+
 	return;	
 }
 
--- usr/src/sys/x64/gdt.h
+++ usr/src/sys/x64/gdt.h
@@ -10,6 +10,25 @@
 #ifndef _X64_GDT_H_
 #define _X64_GDT_H_
 
+/* A segment descriptor is 8 bytes */
+typedef uint64_t Segdesc;
+
+/*
+ * Macros to setup segment descriptors
+ * Bitwise OR these values together
+ */
+
+/* 0=system, 1=code/data */
+#define SEGDESC_TYPE(x) (((Segdesc) x << 12) << 32)
+/* data: 1=write enable, code: 1=read enable */
+#define SEGDESC_RW(x)	(((Segdesc) x << 9) << 32)
+/* Privilege level */
+#define SEGDESC_DPL(x)	(((Segdesc) x << 13) << 32)
+/* Segment present */
+#define SEGDESC_PRES(x)	(((Segdesc) x << 15) << 32)
+/* Long mode? 1=yes */
+#define SEGDESC_LONG(x) (((Segdesc) x << 21) << 32)
+
 void	gdt_init(void);
 
 #endif /* _X64_GDT_H_ */
